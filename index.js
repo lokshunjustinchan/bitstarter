f64aec3b274d7ad9e1dd7fbb4b77a73a2212e818
Justin Chan <lokshunjustin.chan@postgrad.manchester.ac.uk>
2015-05-04 12:06:38 +0000

    Updated index.js v3

--- index.js
+++ index.js
@@ -4,18 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-/*
+
 app.get('/', function(request, response) {
   response.send('Hello World2!')
 })
-*/
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
 
