33d322a92b96cfd0afdc68a8baa8456f03e16609
Justin Chan <lokshunjustin.chan@postgrad.manchester.ac.uk>
2015-05-04 19:50:26 +0000

    finished part2: wireframe and edit html/css

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express')
+var fs = require('fs')
+var buffer = require('buffer')
+var response = require('response')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -6,10 +9,22 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+    var PrintedText = fs.readFileSync('index.html', "utf8")
+    
+	
+  response.send(PrintedText)
 })
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
+/*
+var textToBePrinted = fs.readFile('index.html', 'utf8');
+
+var buffer = new Buffer(100)
+buffer.write("index html from index.html","utf-8")
+buffer.toString("utf-8")
+
+*/
